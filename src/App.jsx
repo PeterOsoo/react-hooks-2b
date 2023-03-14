b8d774724493f0f3b39b2207b4e2cb540d9e8fd8
Peter Osoo <posoo617@gmail.com>
2023-03-14 07:56:24 +0300

    controlled Inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react"
+import ControlledInputs from "./components/ControlledInputs"
 
 const App = () => {
-	return <div>React Hooks</div>
+	return (
+		<div>
+			<h1> React Hooks </h1>
+			<ControlledInputs />
+		</div>
+	)
 }
 
 export default App
