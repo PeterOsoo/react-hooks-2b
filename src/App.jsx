aa371507b4d6ba976799d6ccea06cb6d5556d4ee
Peter Osoo <posoo617@gmail.com>
2023-03-14 08:07:30 +0300

    User challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import ControlledInputs from "./components/ControlledInputs"
+import UserChallenge from "./components/UserChallenge"
 
 const App = () => {
 	return (
 		<div>
-			<h1> React Hooks </h1>
+			<h1 className="container"> React Hooks </h1>
 			<ControlledInputs />
+			<UserChallenge />
 		</div>
 	)
 }
